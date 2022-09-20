9eb02fb8ac07ab638febce056a7bd5ea8607b3b8
hyunsung kim <hyunsungkim@hyunsungui-MacBookPro.local>
2022-09-20 12:31:41 +0900

    Privacy Typo update

--- client/components/privacy/Privacy.tsx
+++ client/components/privacy/Privacy.tsx
@@ -9,7 +9,7 @@ const Privacy: React.FC<Props> = () => {
     <Container>
       <Contents>
         <h4>
-          디엔케이드림('https://paysys.kr' 이하 행사전표시스템)은(는)
+          디엔케이드림(&apos;https://paysys.kr&apos; 이하 행사전표시스템)은(는)
           [개인정보보호법] 제30조에 따라 정보주체의 개인정보를 보호하고 이와
           관련한 고충을 신속하고 원활하게 처리할 수 있도록 하기 위하여 다음과
           같이 개인정보 처리방침을 수립·공개합니다.
