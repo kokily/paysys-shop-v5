4eeafcdcc6151e6640cb4a1cf3f5c6fb7b70cec5
hyunsung kim <hyunsungkim@hyunsungui-MacBookPro.local>
2022-09-20 12:28:40 +0900

    Privacy Page

---
+++ client/pages/privacy.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next';
+import Privacy from '../components/privacy/Privacy';
+
+const PrivacyPage: NextPage = () => {
+  return <Privacy />;
+};
+
+export default PrivacyPage;
