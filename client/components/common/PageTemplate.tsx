3e1d2097af69615484f50ed6a0b1af28b38aa55a
hyunsung kim <hyunsungkim@hyunsungui-MacBookPro.local>
2022-09-30 14:14:21 +0900

    Privacy Design update

--- client/components/common/PageTemplate.tsx
+++ client/components/common/PageTemplate.tsx
@@ -11,16 +11,24 @@ interface Props {
 
 function PageTemplate({ children, user }: Props) {
   return (
-    <>
+    <Layout>
       <Header user={user} />
       <Main>{children}</Main>
       <BottomNav />
-    </>
+    </Layout>
   );
 }
 
 // Styles
+const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
 const Main = styled.main`
+  width: 100%;
+  max-width: 1200px;
   margin: 6rem 5rem 0rem 5rem;
   ${media.medium} {
     margin-left: 1rem;
